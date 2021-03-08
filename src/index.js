c37cc69a4169fcb05dd893103864f43b14d8d120
GermanoGiudici <germano.giudici@gmail.com>
2021-03-08 12:23:49 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,62 @@
+const validateSchema = require('yaml-schema-validator')
+const fs = require('fs')
+const yaml = require('js-yaml')
+
+const BUNDLE_DESCRIPTOR_SCHEMA_PATH = 'src/schemas/bundle-descriptor-schema.yaml'
+const PLUGIN_DESCRIPTOR_SCHEMA_PATH = 'src/schemas/plugin-descriptor-schema.yaml'
+const WIDGET_DESCRIPTOR_SCHEMA_PATH = 'src/schemas/widget-descriptor-schema.yaml'
+const FRAGMENT_DESCRIPTOR_SCHEMA_PATH = 'src/schemas/fragment-descriptor-schema.yaml'
+const PAGETEMPLATE_DESCRIPTOR_SCHEMA_PATH = 'src/schemas/pagetemplate-descriptor-schema.yaml'
+const PAGE_DESCRIPTOR_SCHEMA_PATH = 'src/schemas/page-descriptor-schema.yaml'
+
+const _addPath = func => {
+  return path => {
+    const content = yaml.load(fs.readFileSync(path, 'utf8'))
+    return {path, errors: func(content)}
+  }
+}
+
+const _validateSchema = (fileContent, schema) =>
+  validateSchema(fileContent, {
+    logLevel: 'none',
+    schemaPath: schema
+  })
+
+
+const _validateBundleDescriptorFile = fileContent =>
+  _validateSchema(fileContent, BUNDLE_DESCRIPTOR_SCHEMA_PATH)
+
+const _validatePluginDescriptorFile = fileContent =>
+  _validateSchema(fileContent, PLUGIN_DESCRIPTOR_SCHEMA_PATH)
+
+const _validateWidgetDescriptorFile = fileContent =>
+  _validateSchema(fileContent, WIDGET_DESCRIPTOR_SCHEMA_PATH)
+
+const _validateFragmentDescriptorFile = fileContent =>
+  _validateSchema(fileContent, FRAGMENT_DESCRIPTOR_SCHEMA_PATH)
+
+const _validatePageTemplateDescriptorFile = fileContent =>
+  _validateSchema(fileContent, PAGETEMPLATE_DESCRIPTOR_SCHEMA_PATH)
+
+const _validatePageDescriptorFile = fileContent =>
+  _validateSchema(fileContent, PAGE_DESCRIPTOR_SCHEMA_PATH).filter(item => {
+      const {message, path} = item
+      const noConfigPath = path.indexOf('.config.') === -1
+      const noMessageConfig = message.indexOf('.config.') === -1
+      const noMessage = message.indexOf('is not present in schema"') === -1
+      return noConfigPath && noMessageConfig && noMessage
+    }
+  )
+
+
+module.exports.validateBundleDescriptorFile = _addPath(_validateBundleDescriptorFile)
+module.exports.validatePluginDescriptorFile = _addPath(_validatePluginDescriptorFile)
+module.exports.validateWidgetDescriptorFile = _addPath(_validateWidgetDescriptorFile)
+module.exports.validateFragmentDescriptorFile = _addPath(_validateFragmentDescriptorFile)
+module.exports.validatePagetemplateDescriptorFile = _addPath(_validatePageTemplateDescriptorFile)
+module.exports.validatePageDescriptorFile = _addPath(_validatePageDescriptorFile)
+
+
+
+
+
---
+++ test/validation.test.js
@@ -0,0 +1,44 @@
+const {
+  validateBundleDescriptorFile,
+  validatePluginDescriptorFile,
+  validateWidgetDescriptorFile,
+  validateFragmentDescriptorFile,
+  validatePagetemplateDescriptorFile,
+  validatePageDescriptorFile
+} = require('../src/index')
+
+test('check documentation-bundle-descriptor.yaml', () => {
+  const {path, errors} = validateBundleDescriptorFile('test/file-examples/documentation-bundle-descriptor.yaml');
+  expect(path).toBe('test/file-examples/documentation-bundle-descriptor.yaml')
+  expect(errors).toHaveLength(0)
+})
+test('check bundle-descriptor-broken1.yaml', () => {
+  const {path, errors} = validateBundleDescriptorFile('test/file-examples/bundle-descriptor-broken1.yaml');
+  expect(path).toBe('test/file-examples/bundle-descriptor-broken1.yaml')
+  const expected =  [{"message": "code is required.", "path": "code"}, {"message": "components.plugins must be of type Array.", "path": "components.plugins"}, {"message": "components.plugins is not present in schema", "path": "components.plugins"}]
+  expect(errors).toEqual(expected)
+})
+
+test('check documentation-plugin-descriptor.yaml', () => {
+  const {path, errors} = validatePluginDescriptorFile('test/file-examples/documentation-plugin-descriptor.yaml');
+  expect(path).toBe('test/file-examples/documentation-plugin-descriptor.yaml')
+  expect(errors).toHaveLength(0)
+})
+
+test('check documentation-widget-descriptor.yaml', () => {
+  const {path, errors} = validateWidgetDescriptorFile('test/file-examples/documentation-widget-descriptor.yaml');
+  expect(path).toBe('test/file-examples/documentation-widget-descriptor.yaml')
+  expect(errors).toHaveLength(0)
+})
+
+test('check documentation-fragment-descriptor.yaml', () => {
+  const {path, errors} = validateFragmentDescriptorFile('test/file-examples/documentation-fragment-descriptor.yaml');
+  expect(path).toBe('test/file-examples/documentation-fragment-descriptor.yaml')
+  expect(errors).toHaveLength(0)
+})
+
+test('check documentation-page-descriptor.yaml', () => {
+  const {path, errors} = validatePageDescriptorFile('test/file-examples/documentation-page-descriptor.yaml');
+  expect(path).toBe('test/file-examples/documentation-page-descriptor.yaml')
+  expect(errors).toHaveLength(0)
+})
